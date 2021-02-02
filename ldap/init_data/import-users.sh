b49a36791097fec22cfa9775be84cefc07ce19c1
Daniel Koch <koch@terrestris.de>
2021-02-02 15:17:20 +0100

    Get script path dynamically

--- ldap/init_data/import-users.sh
+++ ldap/init_data/import-users.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 LDAP_HOST=${1:-localhost}
+SCRIPT_PATH=$(dirname $(realpath -s $0))
 
-ldapadd -x -D "cn=admin,dc=mycompany,dc=com" -w admin -H ldap://$LDAP_HOST -f ldap-mycompany.ldif
+ldapadd -x -D "cn=admin,dc=mycompany,dc=com" -w admin -H ldap://$LDAP_HOST -f $SCRIPT_PATH/ldap-mycompany.ldif
